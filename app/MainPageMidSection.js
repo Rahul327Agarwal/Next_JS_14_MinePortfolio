fa4b488145d615743501f55eae0726087bfff8e7
Rahul <agarwalr327@gmail.com>
2023-12-31 15:50:14 +0530

    modified code in main page

--- app/MainPageMidSection.js
+++ app/MainPageMidSection.js
@@ -6,32 +6,22 @@ import Image from "next/image";
 
 export default function MainPageMidSection() {
   const colors = ["#F59E0B", "#84CC16", "#10B981", "#3B82F6"];
+  const AboutMe = ["Developer.", "Designer.", "Programmer.", "Youtuber."];
   return (
     <div className="flex flex-row justify-center items-start overflow-hidden">
       {/* Text container */}
 
       <div className="w-full md:w-1/2 mx-auto text-center md:text-left lg:p-20">
         <RoughNotationGroup show={true}>
-          <RainbowHighlight color={colors[0]}>
-            <h1 className="text-4xl md:text-7xl font-bold text-gray-700 dark:text-gray-200 my-2">
-              Developer.
-            </h1>
-          </RainbowHighlight>
-          <RainbowHighlight color={colors[1]}>
-            <h1 className="text-4xl md:text-7xl font-bold text-gray-700 dark:text-gray-200 my-2">
-              Designer.
-            </h1>
-          </RainbowHighlight>
-          <RainbowHighlight color={colors[2]}>
-            <h1 className="text-4xl md:text-7xl font-bold text-gray-700 dark:text-gray-200 my-2">
-              Programmer.
-            </h1>
-          </RainbowHighlight>
-          <RainbowHighlight color={colors[3]}>
-            <h1 className="text-4xl md:text-7xl font-bold text-gray-700 dark:text-gray-200 my-2">
-              Youtuber.
-            </h1>
-          </RainbowHighlight>
+          {AboutMe.map((role, idx) => {
+            return (
+              <RainbowHighlight color={colors[idx]} key={idx}>
+                <h1 className="text-4xl md:text-7xl font-bold text-gray-700 dark:text-gray-200 my-2">
+                  {role}
+                </h1>
+              </RainbowHighlight>
+            );
+          })}
         </RoughNotationGroup>
       </div>
       {/* Image container */}
